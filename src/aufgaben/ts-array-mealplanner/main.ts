64079296562f24a8463a4f26ff8b765c330b45e5
Mark1U <mark@zurcloud.com>
2024-10-09 09:48:32 +0200

    Meal Planner - Refactor

--- src/aufgaben/ts-array-mealplanner/main.ts
+++ src/aufgaben/ts-array-mealplanner/main.ts
@@ -7,6 +7,11 @@ const mealInput = document.querySelector('#mealInput') as HTMLInputElement;
 const addMealBtn = document.querySelector('#addMealBtn') as HTMLButtonElement;
 const mealsList = document.querySelector('#meals') as HTMLUListElement;
 
+const displayMeals = () => {
+    mealsList.innerHTML = `<h3>Week ${selectedWeekIndex + 1}:</h3>\n`;
+    weekMeals[selectedWeekIndex].forEach(meal => mealsList.innerHTML += `<li>${meal}</li>\n`)
+}
+
 const addMeal = () => {
     const meal = mealInput.value.trim();
     if (meal) {
@@ -18,23 +23,11 @@ const addMeal = () => {
     }
 }
 
-const displayMeals = () => {
-    mealsList.innerHTML = '';
-    // weekMeals.forEach((week, index) => {
-    mealsList.innerHTML += `<h3>Week ${selectedWeekIndex + 1}:</h3>\n`;
-    weekMeals[selectedWeekIndex].forEach(meal => {
-        mealsList.innerHTML += `<li>${meal}</li>\n`;
-    })
-}
-
-
 const updateSelectedWeek = () => {
     selectedWeekIndex = +weekSelector.value;
     displayMeals();
 }
 
 addMealBtn.addEventListener('click', addMeal);
-
 weekSelector.addEventListener('change', updateSelectedWeek);
 
-displayMeals();
